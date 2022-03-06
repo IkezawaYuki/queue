7cbcb2a497a281ecd3ade65ac779bb511833c209
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-07 00:04:59 +0900

    add image

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
 
+
 const Top = () => {
     return (
         <>
             <h1>Top画面</h1>
+            <div>
+              <img src="/logo_top_header.svg" />
+              <p>
+                "Queue(クー)"は、フランス語で「しっぽ」という意味です。
+                 猫にも人にも心地よいをモットーに、猫おもちゃ、雑貨
+                 を作っています。           
+              </p> 
+            </div>
             <section>
-             
+              <img src="/bg_top_header.jpg" />
             </section>
+
+            <section>
+
+            </section> 
         </>
     )
 }
