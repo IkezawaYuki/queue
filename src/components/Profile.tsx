b0960f6875cd31d30708fd256bd08ddabbbcd78c
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-10 23:30:41 +0900

    mod

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,16 +4,16 @@ import React from "react";
 const Profile = () => {
   return (
     <div className="container">
-      <h4 className="text-center">プロフィール</h4>
-      <img className="" src="/img_profile.jpg" />
-      <h5>@teleriaquque</h5>
-      <p>
+      <h4 className="pt-12 text-center">プロフィール</h4>
+      <img className="mx-auto w-32" src="/img_profile.jpg" />
+      <h5 className="text-center">@teleriaquque</h5>
+      <p className="text-center">
         猫雑貨・猫おもちゃ、その他ハンドメイド
         時々農家になります。
       </p>
-      <div className="flex">
-        <img src="sns_twitter.png"/>
-        <img src="sns_insta.png" />
+      <div className="my-12 flex items-center justify-items-center">
+        <img className="mx-auto w-24" src="sns_twitter.png"/>
+        <img className="mx-auto w-24" src="sns_insta.png" />
       </div>
     </div>
   );
