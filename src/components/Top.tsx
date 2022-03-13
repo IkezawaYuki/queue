b409cd95560eb58df3f7673b2a9f1e080a27ba2d
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-13 21:16:22 +0900

    mod

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -17,29 +17,31 @@ const Top = () => {
             <section>
               <img src="/bg_top_header.jpg" />
             </section>
-            <h4 className="text-3xl pt-24 pb-12 font-bold text-center">委託販売先</h4>
-            <p className="text-center">当店は現在以下の3店舗のレンタルボックスにて委託販売をさせていただいております。
-</p>
-            <p className="pb-12 text-center">商品を実際に手にとってお確かめいただけますので、お近くの方はお気軽にお立ち寄り下さい。</p>
-            <div className="flex">  
-              <div><img src="img_rental_kuroneko_douhua.jpg" alt="" /></div>
-              <div>
-                <h4 className="bg-gray-900 text-white">台湾スイーツ豆花専門 黒猫豆花 様</h4>
-                <div>
+            <div className="bg-amber-100">
+              <h4 className="text-3xl pt-24 pb-12 font-bold text-center">委託販売先</h4>
+              <p className="text-center">当店は現在以下の3店舗のレンタルボックスにて委託販売をさせていただいております。
+  </p>
+              <p className="pb-12 text-center">商品を実際に手にとってお確かめいただけますので、お近くの方はお気軽にお立ち寄り下さい。</p>
+            
+              <div className="flex justify-center py-6">  
+              <div className=""><img src="img_rental_kuroneko_douhua.jpg" alt="" /></div>
+              <div className="w-1/2">
+                <h4 className="ml-6 py-2 pl-4 text-xl font-bold bg-gray-900 text-white">台湾スイーツ豆花専門 黒猫豆花 様</h4>
+                <div className="mt-8 ml-6">
                   <div className="flex">
-                    <p className="w-1/5">住所</p>
+                    <p className="w-1/5 font-bold">住所</p>
                     <p className="w-4/5">神奈川県川崎市高津区二子2丁目7-40　フォーレスト多摩川102</p>
                   </div>
                   <div className="flex">
-                    <p className="w-1/5">営業時間</p>
+                    <p className="w-1/5 font-bold">営業時間</p>
                     <p className="w-4/5">10:00-21:00（時短営業中は20:00まで）</p>
                   </div>
                   <div className="flex">
-                    <p className="w-1/5">定休日</p>
+                    <p className="w-1/5 font-bold">定休日</p>
                     <p className="w-4/5">水曜日</p>
                   </div>
                   <div className="flex">
-                    <p className="w-1/5">website</p>
+                    <p className="w-1/5 font-bold">website</p>
                     <p className="w-4/5">https://kuroneko-douhua.com/</p>
                   </div>
                 </div>
@@ -54,6 +56,9 @@ const Top = () => {
               <div><img src="img_rental_nekoshiki.jpg" alt="" /></div>
               <div></div>
             </div>
+
+            </div>
+
             
 
             <h4 className="pt-12 text-center">プロフィール</h4>
