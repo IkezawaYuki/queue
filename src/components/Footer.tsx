126ceca43543b8a85324155db5430ca9b0d65230
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-14 23:14:08 +0900

    mod

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-96 bg-yellow-900 text-white">
-      <img src="/logo_footer.svg" className="mx-auto pt-6 h-36 w-auto"/>
+    <div className="bg-yellow-900 text-white">
+      <img src="/logo_footer.svg" className="mx-auto pt-8 h-28 w-auto"/>
       <ul className="m-2 flex justify-center divide-x divide-solid">
-        <li><a className="mx-4" href="#">トップページ</a></li>  
-        <li><a className="mx-4" href="#">商品一覧</a></li>  
-        <li><a className="mx-4" href="#">ねこブログ</a></li>  
-        <li><a className="mx-4" href="#">ねこ店員紹介</a></li>  
-        <li><a className="mx-4" href="#">よくあるご質問</a></li>  
-        <li><a className="mx-4" href="#">お問合せ</a></li>  
+        <li><a className="mx-4 text-xs" href="#">トップページ</a></li>  
+        <li><a className="mx-4 text-xs" href="#">商品一覧</a></li>  
+        <li><a className="mx-4 text-xs" href="#">ねこブログ</a></li>  
+        <li><a className="mx-4 text-xs" href="#">ねこ店員紹介</a></li>  
+        <li><a className="mx-4 text-xs" href="#">よくあるご質問</a></li>  
+        <li><a className="mx-4 text-xs" href="#">お問合せ</a></li>  
       </ul>
       <ul className="flex justify-center pt-4 divide-x divide-solid">
-        <li><a className="mx-4" href="#">特定商取引法に関する表記</a></li>  
-        <li><a className="mx-4" href="#">プライバシーポリシー</a></li>  
+        <li><a className="mx-4 text-xs" href="#">特定商取引法に関する表記</a></li>  
+        <li><a className="mx-4 text-xs" href="#">プライバシーポリシー</a></li>  
       </ul>
-      <div className="flex justify-center pt-12">©️2022 yi quque</div>
+      <div className="flex justify-center pt-12 text-xs pb-12">©️2022 yi quque</div>
     </div>
   )
 }
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -17,6 +17,16 @@ const Top = () => {
             <section>
               <img src="/bg_top_header.jpg" />
             </section>
+
+            <div className="flex mx-48 mt-24">
+              <div className="w-8"><img src="ico_cat.svg" alt="" /></div>
+              <h4 className="py-4 w-1/2 ml-3 mr-24 text-3xl font-bold">お知らせ</h4>
+              <div className="">
+                <div className="py-4"><button className="bg-slate-800 text-white px-4 py-1 rounded">一覧へ</button></div>
+              </div>
+            </div>
+
+
             <div className="bg-amber-100">
               <h4 className="text-3xl pt-24 pb-12 font-bold text-center">委託販売先</h4>
               <p className="text-center">当店は現在以下の3店舗のレンタルボックスにて委託販売をさせていただいております。
@@ -24,53 +34,94 @@ const Top = () => {
               <p className="pb-12 text-center">商品を実際に手にとってお確かめいただけますので、お近くの方はお気軽にお立ち寄り下さい。</p>
             
               <div className="flex justify-center py-6">  
-              <div className=""><img src="img_rental_kuroneko_douhua.jpg" alt="" /></div>
-              <div className="w-1/2">
-                <h4 className="ml-6 py-2 pl-4 text-xl font-bold bg-gray-900 text-white">台湾スイーツ豆花専門 黒猫豆花 様</h4>
-                <div className="mt-8 ml-6">
-                  <div className="flex">
-                    <p className="w-1/5 font-bold">住所</p>
-                    <p className="w-4/5">神奈川県川崎市高津区二子2丁目7-40　フォーレスト多摩川102</p>
+                <div className=""><img src="img_rental_kuroneko_douhua.jpg" alt="" /></div>
+                <div className="w-1/2">
+                  <h4 className="ml-6 py-2 pl-4 text-xl font-bold bg-gray-900 text-white">台湾スイーツ豆花専門 黒猫豆花 様</h4>
+                  <div className="mt-8 ml-6">
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">住所</p>
+                      <p className="w-4/5">神奈川県川崎市高津区二子2丁目7-40　フォーレスト多摩川102</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">営業時間</p>
+                      <p className="w-4/5">10:00-21:00（時短営業中は20:00まで）</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">定休日</p>
+                      <p className="w-4/5">水曜日</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">website</p>
+                      <p className="w-4/5">https://kuroneko-douhua.com/</p>
+                    </div>
                   </div>
-                  <div className="flex">
-                    <p className="w-1/5 font-bold">営業時間</p>
-                    <p className="w-4/5">10:00-21:00（時短営業中は20:00まで）</p>
-                  </div>
-                  <div className="flex">
-                    <p className="w-1/5 font-bold">定休日</p>
-                    <p className="w-4/5">水曜日</p>
-                  </div>
-                  <div className="flex">
-                    <p className="w-1/5 font-bold">website</p>
-                    <p className="w-4/5">https://kuroneko-douhua.com/</p>
-                  </div>
+                  
                 </div>
-                
               </div>
-            </div>
-            <div>
-              <div><img src="img_rental_mayscafe.jpg" alt="" /></div>
-              <div></div>
-            </div>
-            <div>
-              <div><img src="img_rental_nekoshiki.jpg" alt="" /></div>
-              <div></div>
-            </div>
-
+              <div className="flex justify-center py-6">  
+                <div className=""><img src="img_rental_mayscafe.jpg" alt="" /></div>
+                <div className="w-1/2">
+                  <h4 className="ml-6 py-2 pl-4 text-xl font-bold bg-gray-900 text-white">May’s Cafe -ハンドメイド雑貨＆CAFE-  様</h4>
+                  <div className="mt-8 ml-6">
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">住所</p>
+                      <p className="w-4/5">東京都世田谷区太子堂5-17-20-1F</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">営業時間</p>
+                      <p className="w-4/5">土日祝日11:00〜19:00のみ営業中</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">定休日</p>
+                      <p className="w-4/5">水曜日</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">Instagram</p>
+                      <p className="w-4/5">https://www.instagram.com/mays._.cafe/</p>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+              <div className="flex justify-center py-6">  
+                <div className=""><img src="img_rental_nekoshiki.jpg" alt="" /></div>
+                <div className="w-1/2">
+                  <h4 className="ml-6 py-2 pl-4 text-xl font-bold bg-gray-900 text-white">台湾スイーツ豆花専門 黒猫豆花 様</h4>
+                  <div className="mt-8 ml-6">
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">住所</p>
+                      <p className="w-4/5">神奈川県川崎市高津区二子2丁目7-40　フォーレスト多摩川102</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">営業時間</p>
+                      <p className="w-4/5">10:00-21:00（時短営業中は20:00まで）</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">定休日</p>
+                      <p className="w-4/5">水曜日</p>
+                    </div>
+                    <div className="flex my-2">
+                      <p className="w-1/5 font-bold">website</p>
+                      <p className="w-4/5">https://kuroneko-douhua.com/</p>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
             </div>
 
             
 
-            <h4 className="pt-12 text-center">プロフィール</h4>
-            <img className="mx-auto w-32" src="/img_profile.jpg" />
-            <h5 className="text-center">@teleriaquque</h5>
-            <p className="w-48 mx-auto text-center">
+            <h4 className="text-3xl font-bold pt-24 text-center">プロフィール</h4>
+            <img className="py-4 mx-auto w-32" src="/img_profile.jpg" />
+            <h5 className="text-center font-bold pb-4">@teleriaquque</h5>
+            <p className="w-96 mx-auto text-center">
               猫雑貨・猫おもちゃ、その他ハンドメイド
               時々農家になります。
             </p> 
-            <div className="flex justify-center">
-              <div><img className="my-10 mx-2 mx-auto w-16" src="sns_twitter.png"/></div>
-              <div><img className="my-10 mx-2 mx-auto w-16" src="sns_insta.png" /></div>
+            <div className="flex justify-center mb-16">
+              <div><img className="my-10 mx-4 mx-auto w-16" src="sns_twitter.png"/></div>
+              <div><img className="my-10 mx-4 mx-auto w-16" src="sns_insta.png" /></div>
             </div>
 
             <Footer />
