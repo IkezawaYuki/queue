610dd0282da7f2f82425b46761799079808273ae
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-24 23:35:16 +0900

    mod

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -25,7 +25,7 @@ const Products = () => {
           </ul>
         </div>
         
-        <div className="flex justify-center mb-24">
+        <div className="flex justify-center">
           <div className="mt-12 mx-3 border-2 border-solid border-orange-300">
             <div className="mx-6 mt-6 w-80">
               <img src="/dmy_pd001.jpg" alt="" />
@@ -55,7 +55,7 @@ const Products = () => {
           </div>
         </div>
 
-        <div className="flex justify-center mb-48">
+        <div className="flex justify-center">
           <div className="mt-12 mx-3 border-2 border-solid border-orange-300">
             <div className="mx-6 mt-6 w-80">
               <img src="/dmy_pd004.jpg" alt="" />
@@ -83,6 +83,48 @@ const Products = () => {
               <div className="text-center py-2 border boader-solid border-green-600 rounded-md my-2"><a className="block" href="#">詳細を見る</a></div>
             </div>
           </div>
+        </div>
+
+        <div className="flex justify-center">
+          <div className="mt-12 mx-3 border-2 border-solid border-orange-300">
+            <div className="mx-6 mt-6 w-80">
+              <img src="/dmy_pd007.jpg" alt="" />
+              <h4 className="border-dotted mt-4 border-gray-300 border-b text-lg font-bold">【焼印付】爪とぎ型キーホルダー</h4>
+              <p className="h-12 text-sm mt-4">可愛らしいミニチュアサイズの「猫の爪とぎ」型キーホルダー。木製ケースの裏面には猫シルエットの焼印付きでオシャレ！</p>
+              <div className="mt-8 ml-auto w-24 font-bold">1000円<span className="text-xs font-normal">(税込)</span></div>
+              <div className="text-center py-2 border boader-solid border-green-600 rounded-md my-2"><a className="block" href="#">詳細を見る</a></div>
+            </div>
+          </div>
+          <div className="mt-12 mx-3 border-2 border-solid border-orange-300">
+            <div className="mx-6 mt-6 w-80">
+              <img src="/dmy_pd008.jpg" alt="" />
+              <h4 className="border-dotted mt-4 border-gray-300 border-b text-lg font-bold">肉球ブローチ（7個セット）</h4>
+              <p className="h-12 text-sm mt-4">定番の肉球型のレジンブローチ（7個セット）です。</p>
+              <div className="mt-8 ml-auto w-24 font-bold">1000円<span className="text-xs font-normal">(税込)</span></div>
+              <div className="text-center py-2 border boader-solid border-green-600 rounded-md my-2"><a className="block" href="#">詳細を見る</a></div>
+            </div>
+          </div>
+          <div className="mt-12 mx-3 border-2 border-solid border-orange-300">
+            <div className="mx-6 mt-6 w-80">
+              <img src="/dmy_pd009.jpg" alt="" />
+              <h4 className="border-dotted mt-4 border-gray-300 border-b text-lg font-bold">レース猫首輪（鈴付き）</h4>
+              <p className="h-12 text-sm mt-4">アースカラーのレース＋アンティーク調の金具でシックな印象の猫首輪です。</p>
+              <div className="mt-8 ml-auto w-24 font-bold">1000円<span className="text-xs font-normal">(税込)</span></div>
+              <div className="text-center py-2 border boader-solid border-green-600 rounded-md my-2"><a className="block" href="#">詳細を見る</a></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-24 justify-center flex">
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">＜ 前へ</a>
+          <a className="px-3 py-1 m-1 bg-orange-200 rounded" href="#">1</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">2</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">3</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">4</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">5</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">6</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">7</a>
+          <a className="px-3 py-1 m-1 bg-orange-100 rounded" href="#">次へ ＞</a>
         </div>
 
       <Footer />
