15a04f327dacce40b85efd4db3d5da78de1c7474
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-25 23:28:34 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Top from "./components/Top"
 import Products from './components/Products';
 import "./index.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Details from './components/Details';
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Top />} />
           <Route path="/products" element={<Products />} />
+          <Route path='/details' element={<Details/>} />
         </Routes>
       </BrowserRouter>
     </>
