c3f61bdd1640560bb27c549bf68246826a5e70cc
IkezawaYuki <y.ikezawa93@gmail.com>
2022-03-26 23:44:48 +0900

    mod

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./Footer";
 import Header from "./Header";
 
 
@@ -6,6 +7,18 @@ const Details = () => {
   return (
     <>
       <Header/>
+        <div className="flex">
+          <div>
+            <div><img src="dmy_pd001.jpg" alt="" /></div>
+            <div className="flex">
+              <img className="w-24 h-24" src="dmy_pd001.jpg" alt="" />
+              <img src="dmy_pd001_02.jpg" alt="" />
+              <img src="dmy_pd001_03.jpg" alt="" />
+            </div>
+          </div>
+          <div></div>
+        </div>
+      <Footer/>
     </>
   );
 }
